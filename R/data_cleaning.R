9dec649339062347911e2f83d61b83769e996cfe
Freddie J. Heather <30920629+FreddieJH@users.noreply.github.com>
2023-02-21 10:56:16 +1100

    replaces native pipe

--- R/data_cleaning.R
+++ R/data_cleaning.R
@@ -11,27 +11,27 @@ library(arrow)
 
 # Observation level data (survey_id, species_name, size)
 if(!file.exists("data/cleaned/obs_data_m1_aus.parquet")){
-  read_csv("data/raw/ep_m1_ALL.csv") |> 
-    filter(country == "Australia") |> 
-    filter(method == 1) |> 
-    filter(size_class > 0) |> 
+  read_csv("data/raw/ep_m1_ALL.csv") %>% 
+    filter(country == "Australia") %>% 
+    filter(method == 1) %>% 
+    filter(size_class > 0) %>% 
     group_by(
       survey_id, 
       species_name,
       size_class
-    ) |> 
+    ) %>% 
     summarise(total = sum(total),
-              .groups = "drop") |> 
-    uncount(weights = total) |> 
-    filter(str_detect(species_name, "[A-Z]{1}[a-z]+\\s[a-z]+")) |> 
-    filter(!str_detect(species_name, "\\.")) |> 
+              .groups = "drop") %>% 
+    uncount(weights = total) %>% 
+    filter(str_detect(species_name, "[A-Z]{1}[a-z]+\\s[a-z]+")) %>% 
+    filter(!str_detect(species_name, "\\.")) %>% 
     write_parquet("data/cleaned/obs_data_m1_aus.parquet")
 }
 
 # survey-level data (survey_id, site_code, lat, lon etc.)
 if(!file.exists("data/cleaned/survey_list_m1_aus.parquet")){
-  read_csv("data/raw/ep_m1_ALL.csv") |> 
-    filter(country == "Australia") |> 
+  read_csv("data/raw/ep_m1_ALL.csv") %>% 
+    filter(country == "Australia") %>% 
     select(
       survey_id,
       site_code,
@@ -40,7 +40,7 @@ if(!file.exists("data/cleaned/survey_list_m1_aus.parquet")){
       longitude, 
       depth, 
       survey_date
-    )|> 
+    )%>% 
     write_parquet("data/cleaned/survey_list_m1_aus.parquet")
   
 }
